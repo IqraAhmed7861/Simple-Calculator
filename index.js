e275fe241f9800df83f131aaa3e9309134fe68fd
IqraAhmed7861 <iqraahmedmandvia@gmail.com>
2025-01-11 02:46:53 +0500

    Add files via upload
    completed

---
+++ index.js
@@ -0,0 +1,20 @@
+function addition() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1) + Number(num2);
+}
+function subtraction() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1) - Number(num2);
+}
+function multiply() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1) * Number(num2);
+}
+function division() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    document.getElementById("result").innerHTML = Number(num1) / Number(num2);
+}
